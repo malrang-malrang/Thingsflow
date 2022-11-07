5574a8d4eba789e159120719f936d6f9f0d0eb7c
malrang <ruehd7924@gmail.com>
2022-11-07 15:15:33 +0900

    ➕[add]: Requestable 추가및 정의 #3

---
+++ Thingsflow/Thingsflow/Utils/Extension/Encodable+Sugar.swift
@@ -0,0 +1,22 @@
+//
+//  Encodable+Sugar.swift
+//  Thingsflow
+//
+//  Created by 김동욱 on 2022/11/07.
+//
+
+import Foundation
+
+extension Encodable {
+    func toDictionary() -> Result<[String: Any], NetworkError> {
+        guard let jsonData = try? Json.encoder.encode(self) else {
+            return .failure(.encodeError)
+        }
+
+        guard let dictionaryData = try? JSONSerialization.jsonObject(with: jsonData) as? [String: Any] else {
+            return .failure(.encodeError)
+        }
+
+        return .success(dictionaryData)
+    }
+}
---
+++ Thingsflow/Thingsflow/Utils/Singleton/Json.swift
@@ -0,0 +1,15 @@
+//
+//  Json.swift
+//  Thingsflow
+//
+//  Created by 김동욱 on 2022/11/07.
+//
+
+import Foundation
+
+struct Json {
+    static let encoder = JSONEncoder()
+    static let decoder = JSONDecoder()
+
+    private init() {}
+}
